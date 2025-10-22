9c10f6f64ed14b38da188030e081b05cdcd5f96e
Jens Gotfredsen <somewhereinnowhere@live.dk>
2025-10-22 10:53:10 +0200

    Add files via upload

---
+++ docs/CREATE_wishlist.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS wishlist_project;
+	CREATE DATABASE wishlist_project
+    DEFAULT CHARACTER SET utf8mb4;
+USE wishlist_project;
+
+CREATE TABLE User (
+	id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    password VARCHAR(255) NOT NULL,
+    mail VARCHAR(100) NOT NULL UNIQUE,
+    firstname VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
+    
+    PRIMARY KEY(id)
+);
---
+++ docs/INSERT_wishlist.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO User (password, mail, firstname, surname) VALUES ("1234", "somewhereinnowhere@live.dk", "Jens", "Gotfredsen"),
+	("4321", "jens_gotfredsen@icloud.com", "Luca", "Di Leo")
