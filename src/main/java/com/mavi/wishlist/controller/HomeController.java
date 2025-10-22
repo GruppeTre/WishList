0247583713f51e5b1110345af4721057b91a1fab
MaVirgil <magnusvirgil@hotmail.com>
2025-10-22 10:58:34 +0200

    Moved index endpoint to own controller

--- src/main/java/com/mavi/wishlist/controller/HomeController.java
+++ src/main/java/com/mavi/wishlist/controller/HomeController.java
@@ -2,9 +2,12 @@ package com.mavi.wishlist.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
 
 @Controller
-public class UserController {
+public class HomeController {
 
+    @GetMapping("/")
+    public String getIndex(){
+        return "index";
+    }
 }
