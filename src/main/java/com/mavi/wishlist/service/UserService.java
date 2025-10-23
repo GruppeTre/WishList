6566ccb7c150baa603b6b6adc1d642d90d658cba
MaVirgil <magnusvirgil@hotmail.com>
2025-10-23 20:19:11 +0200

    added registerUser method

--- src/main/java/com/mavi/wishlist/service/UserService.java
+++ src/main/java/com/mavi/wishlist/service/UserService.java
@@ -38,4 +38,55 @@ public class UserService {
     public boolean mailIsTaken(String mail) {
         return getUserByMail(mail) != null;
     }
+
+    public User registerUser(User user) {
+
+        //trim mail for leading and trailing whitespaces
+        user.setMail(user.getMail().trim());
+
+        //check for validity (no empty fields)
+        if (!isValidNewUser(user)) {
+            return null;
+        }
+
+        return userRepository.addUser(user);
+    }
+
+    //collection of guard clauses to run before adding new user to database
+    private boolean isValidNewUser(User user) {
+
+        int passwordMinLength = 4;
+
+        boolean mailIsBlank = user.getMail().isBlank();
+        if (mailIsBlank) {
+            return false;
+        }
+
+        boolean passwordIsBlank = user.getPassword().isBlank();
+        if (passwordIsBlank) {
+            return false;
+        }
+
+        boolean firstNameIsBlank = user.getFirstName().isBlank();
+        if (firstNameIsBlank) {
+            return false;
+        }
+
+        boolean lastNameIsBlank = user.getLastName().isBlank();
+        if (lastNameIsBlank) {
+            return false;
+        }
+
+        boolean passwordIsTooShort = user.getPassword().length() < passwordMinLength;
+        if (passwordIsTooShort) {
+            return false;
+        }
+
+        boolean mailIsTaken = mailIsTaken(user.getMail());
+        if (mailIsTaken) {
+            return false;
+        }
+
+        return true;
+    }
 }
