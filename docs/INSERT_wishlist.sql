df01ded16d59561821da6eb12bf01f394e7fb242
Jens Gotfredsen <somewhereinnowhere@live.dk>
2025-10-28 13:21:38 +0100

    Update INSERT_wishlist.sql
    added hashed & salted password

--- docs/INSERT_wishlist.sql
+++ docs/INSERT_wishlist.sql
@@ -1,1 +1,2 @@
-INSERT IGNORE INTO User (password, mail, firstName, lastName) VALUES ("1234", "test@example.com", "Peter", "Petersen")
+INSERT IGNORE INTO User (password, mail, firstName, lastName) VALUES ("'2', '$argon2id$v=19$m=16384,t=2,p=1$47/zujuiWlJWAVTgjhvyHw$sZd2STT+9+Ms4OOrKTcn+Z/GNquAqcmKqcEIBNwGZbw', 'test@example.com', 'Peter', 'Petersen'
+", "test@example.com", "Peter", "Petersen")
