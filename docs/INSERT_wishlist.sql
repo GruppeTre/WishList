51c2930f1a4be0edfc1303f05068dc70a4c32d03
Magnus Sørensen <magnusvirgil@hotmail.com>
2025-10-28 16:15:29 +0100

    Added admin user in inset script (pw: admin)

--- docs/INSERT_wishlist.sql
+++ docs/INSERT_wishlist.sql
@@ -1,2 +1,1 @@
-INSERT IGNORE INTO User (password, mail, firstName, lastName) VALUES ("'2', '$argon2id$v=19$m=16384,t=2,p=1$47/zujuiWlJWAVTgjhvyHw$sZd2STT+9+Ms4OOrKTcn+Z/GNquAqcmKqcEIBNwGZbw', 'test@example.com', 'Peter', 'Petersen'
-", "test@example.com", "Peter", "Petersen")
+INSERT IGNORE INTO User (password, mail, firstName, lastName) VALUES INSERT IGNORE INTO User (password, mail, firstName, lastName) VALUES ('$argon2id$v=19$m=16384,t=2,p=1$Hkk7F7M71WIJjuw8uym1wg$lMWg2Vv6DnISI6YlpKkC6CjzcF45iBKbfHck+kORYmg', 'admin@admin', 'Test', 'Name');
