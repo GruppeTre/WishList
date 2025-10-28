f50416eab17e135bff1a78bdef7ca62b008c50cd
Jens Gotfredsen <somewhereinnowhere@live.dk>
2025-10-28 11:37:24 +0100

    Update CREATE_wishlist.sql
    reordered wish to be above wishlist

--- docs/CREATE_wishlist.sql
+++ docs/CREATE_wishlist.sql
@@ -8,7 +8,15 @@ CREATE TABLE User (
     password VARCHAR(255) NOT NULL,
     mail VARCHAR(100) NOT NULL UNIQUE,
     firstName VARCHAR(50) NOT NULL,
-    surName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE wish (
+	id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    link VARCHAR(255) NOT NULL,
     
     PRIMARY KEY(id)
 );
@@ -22,12 +30,4 @@ CREATE TABLE wishlist (
 		ON DELETE CASCADE,
 	FOREIGN KEY(wish_id) REFERENCES Wish(id)
 		ON DELETE CASCADE
-);
-
-CREATE TABLE wish (
-	id INT NOT NULL UNIQUE AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    link VARCHAR(255) NOT NULL,
-    
-    PRIMARY KEY(id)
 );
