ee5a212d4d72084c96723c733d81caf4902fde82
WiiTee <somewhereinnowhere@live.dk>
2025-11-04 11:56:26 +0100

    added shouldGetUserById, shouldAddUser, shouldUpdateUser

--- src/test/java/com/mavi/wishlist/repository/UserRepositoryTest.java
+++ src/test/java/com/mavi/wishlist/repository/UserRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.mavi.wishlist.repository;
 
 import com.mavi.wishlist.model.User;
+import com.mavi.wishlist.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,5 +37,46 @@ class UserRepositoryTest {
         assertThat(user.getPassword()).isEqualTo("adam1234");
         assertThat(user.getFirstName()).isEqualTo("Adam");
         assertThat(user.getLastName()).isEqualTo("Adamsen");
+        //assertThat().isEqualTo("80ee1e11b3b393efd0872de7");
+    }
+
+    @Test
+    void shouldGetUserById() throws Exception {
+        User user = repository.getUserById(1);
+
+        assertThat(user.getMail()).isEqualTo("adam@mail.dk");
+        assertThat(user.getId()).isEqualTo(1);
+        assertThat(user.getPassword()).isEqualTo("adam1234");
+        assertThat(user.getFirstName()).isEqualTo("Adam");
+        assertThat(user.getLastName()).isEqualTo("Adamsen");
+    }
+
+    @Test
+    void shouldAddUser() throws Exception {
+        User newUser = new User();
+        newUser.setFirstName("Peter");
+        newUser.setLastName("Petersen");
+        newUser.setMail("peter@peter");
+        newUser.setPassword("peter1234");
+
+        String refString = "0b0609f01458afb981cfeead";
+
+        repository.addUser(newUser, refString);
+
+        User user = repository.getUser(newUser.getMail());
+
+        assertThat(user).isNotNull();
+    }
+
+    @Test
+    void shouldUpdateUser() throws Exception {
+        User user = repository.getUser("adam@adam");
+        user.setMail("adam@testCase");
+
+        repository.updateUser(user);
+
+        User updatedUser = repository.getUser("adam@testCase");
+
+        assertThat(updatedUser.getMail()).isEqualTo("adam@testCase");
     }
 }
--- src/test/resources/h2init.sql
+++ src/test/resources/h2init.sql
@@ -6,6 +6,7 @@ CREATE TABLE account (
                     mail VARCHAR(100) NOT NULL UNIQUE,
                     firstName VARCHAR(50) NOT NULL,
                     lastName VARCHAR(50) NOT NULL,
+                    refString VARCHAR(24) NOT NULL,
 
                     PRIMARY KEY(id)
 );
