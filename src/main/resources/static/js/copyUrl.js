4be37b77fa613210a003d239aedea6c73e64b24b
MaVirgil <magnusvirgil@hotmail.com>
2025-11-04 14:33:09 +0100

    moved JS scripts to dedicated directory

---
+++ src/main/resources/static/js/copyUrl.js
@@ -0,0 +1,20 @@
+function copyUrl() {
+    // Get the text field
+    let copyText = document.getElementById("listLink");
+
+    // Select the text field
+    copyText.select();
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+
+    let alertText = "";
+    if(navigator.language.includes("da")) {
+        alertText = "Link kopieret: "
+    } else{
+        alertText = "Copied to clipboard: "
+    }
+
+    // Alert the copied text
+    alert(alertText + copyText.value);
+}
